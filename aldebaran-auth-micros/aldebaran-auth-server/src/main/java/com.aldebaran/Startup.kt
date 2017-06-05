adac0adc1719d7ed447a12d41bef9df6eba302f3
Peter Pincak <peter.pincak@hotmail.com>
2017-06-05 19:39:06 +0200

    added new gradle modules

--- aldebaran-auth-micros/aldebaran-auth-server/src/main/java/com.aldebaran/Startup.kt
+++ aldebaran-auth-micros/aldebaran-auth-server/src/main/java/com.aldebaran/Startup.kt
@@ -1,5 +1,9 @@
 package com.aldebaran
 
+import com.aldebaran.config.SpringConfiguration
+import org.springframework.boot.SpringApplication
+
+
 fun main(args: Array<String>) {
-   
+    SpringApplication.run(SpringConfiguration::class.java)
 }
