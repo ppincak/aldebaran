5cd5dfcd99cd385cd215bbb0d32795e88d7e33f1
Peter Pincak <peter.pincak@hotmail.com>
2017-06-25 18:20:47 +0200

    small dist fixes

--- dist/digital-ocean/run.sh
+++ dist/digital-ocean/run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-(cd ../../aldebaran-auth-micro/; ./gradlew bootRepackage)
+(cd ../../aldebaran-auth-micro/; chmod +x ./gradlew; ./gradlew bootRepackage)
 
 cp ../../aldebaran-auth-micro/aldebaran-auth-server/build/libs/aldebaran-auth-server-1.0-SNAPSHOT.jar ./aldebaran-auth
 echo "Moved aldebaran-auth-server-1.0-SNAPSHOT.jar"
 cp ../../aldebaran-auth-micro/Dockerfile ./aldebaran-auth
 echo "Moved aldebaran-auth Dockerfile"
 
-(cd ../../aldebaran-orders-micro/; ./gradlew bootRepackage)
+(cd ../../aldebaran-orders-micro/; chmod +x ./gradlew; ./gradlew bootRepackage)
 
 cp ../../aldebaran-orders-micro/aldebaran-order-server/build/libs/aldebaran-order-server-1.0-SNAPSHOT.jar ./aldebaran-order
 echo "Moved aldebaran-order-server-1.0-SNAPSHOT.jar"
