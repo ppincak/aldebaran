532bfcd4c9f7fcf207a6c49b688331318df893df
Peter Pincak <peter.pincak@globallogic.com>
2017-06-26 17:31:42 +0200

    added missing metrics configuration

--- aldebaran-auth-micro/aldebaran-auth-server/src/main/java/com/aldebaran/auth/server/Startup.kt
+++ aldebaran-auth-micro/aldebaran-auth-server/src/main/java/com/aldebaran/auth/server/Startup.kt
@@ -1,9 +1,12 @@
 package com.aldebaran.auth.server
 
 import com.aldebaran.auth.server.config.SpringConfiguration
+import io.prometheus.client.hotspot.DefaultExports
 import org.springframework.boot.SpringApplication
 
 
 fun main(args: Array<String>) {
+    DefaultExports.initialize()
+
     SpringApplication.run(SpringConfiguration::class.java)
 }
